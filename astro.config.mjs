02006af3a6eed39c22a49af0dbfa829cfe6bf93b
timbonnell <tim@bonnell.dev>
2026-05-29 23:36:06 -0230

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default {
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
-};
+export default defineConfig({
+  integrations: [icon()],
+});
